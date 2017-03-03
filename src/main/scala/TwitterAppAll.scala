d74d4901393d16a6078452ac877d7fcedaeaf999
ekrich <ekrichardson@gmail.com>
2017-03-03 16:46:08 -0500

    Update libs and fix type warning

--- src/main/scala/TwitterAppAll.scala
+++ src/main/scala/TwitterAppAll.scala
@@ -59,11 +59,11 @@ object TwitterAppAll {
     
     def toBit(b: Boolean): Int = if (b) 1 else 0
     def withUrl[T <: URLEntity](urls: Array[T]): Boolean = urls.length > 0
-    def reduceByKey[K,V](collection: Traversable[Tuple2[K, V]])(implicit num: Numeric[V]) = {
+    def reduceByKey[K,V](collection: Traversable[Tuple2[K,V]])(implicit num: Numeric[V]): Map[K,V] = {
       import num._
       collection
       .groupBy(_._1)
-      .map { case (group: K, traversable) => traversable.reduce{(a,b) => (a._1, a._2 + b._2)} }
+      .map { case (group, traversable) => traversable.reduce{(a,b) => (a._1, a._2 + b._2)} }
     }
     
     val startTime = sc.startTime
